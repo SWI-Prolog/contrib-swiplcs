f8e77655bff9b2486014ab378efc55b652016e2e
Uwe Lesta <Lesta@SBS-Softwaresysteme.de>
2014-04-25 15:21:03 +0200

    Commit sample projects

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿/*********************************************************
+* 
+*  Author:        Uwe Lesta
+*  Copyright (C): 2009, Uwe Lesta SBS-Softwaresysteme GmbH
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 2.1 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+*  Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+*********************************************************/
+
+// the part below is part of the documentation
+#region demo_doc
+using System;
+using SbsSW.SwiPlCs;
+
+namespace HelloWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"the_PATH_to_boot32.prc");  // or boot64.prc
+            if (!PlEngine.IsInitialized)
+            {
+                String[] param = { "-q" };  // suppressing informational and banner messages
+                PlEngine.Initialize(param);
+                PlQuery.PlCall("assert(father(martin, inka))");
+                PlQuery.PlCall("assert(father(uwe, gloria))");
+                PlQuery.PlCall("assert(father(uwe, melanie))");
+                PlQuery.PlCall("assert(father(uwe, ayala))");
+                using (PlQuery q = new PlQuery("father(P, C), atomic_list_concat([P,' is_father_of ',C], L)"))
+                {
+                    foreach (PlQueryVariables v in q.SolutionVariables)
+                        Console.WriteLine(v["L"].ToString());
+
+                    Console.WriteLine("all children from uwe:");
+                    q.Variables["P"].Unify("uwe");
+                    foreach (PlQueryVariables v in q.SolutionVariables)
+                        Console.WriteLine(v["C"].ToString());
+                }
+                PlEngine.PlCleanup();
+                Console.WriteLine("finshed!");
+            }
+        }
+    }
+}
+#endregion demo_doc
---
+++ HelloWorldDemoFs/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.net
+open System
+open SbsSW.SwiPlCs;
+
+let ple = PlEngine.IsInitialized in
+       if ple then printfn "Echec initialisation" else
+         begin
+             PlEngine.Initialize([|"-q"|])
+             PlQuery.PlCall("assert(father(martin, inka))") |> ignore
+             PlQuery.PlCall("assert(father(uwe, gloria))") |> ignore
+             PlQuery.PlCall("assert(father(uwe, melanie))") |> ignore
+             PlQuery.PlCall("assert(father(uwe, ayala))") |> ignore
+
+             let q = new PlQuery "father(P, C), atomic_list_concat([P,'is_father_of ',C], L)" in
+             begin
+                 Seq.iter (fun (x : PlQueryVariables)  -> printfn "%A" (x.Item("L").Name)) q.SolutionVariables
+
+                 printfn "all children from uwe:"
+                 let r : PlQueryVariables = q.Variables in r.Item("P").Unify("uwe") |> ignore
+                 Seq.iter (fun (x : PlQueryVariables)  -> printfn "%A" (x.Item("C").Name)) q.SolutionVariables
+             end
+
+             PlEngine.PlCleanup()
+             printfn "%A" "finished!"
+         end
+
